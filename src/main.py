fdf4516312868275f06063e4bfb1822ad44a644b
luporojo <alejoyerban112@gmail.com>
2024-06-12 14:00:59 -0300

    Ya andan las imagenes

--- src/main.py
+++ src/main.py
@@ -7,17 +7,19 @@ pygame.init() # Inicia todos los modulos (.display, .mixer, etc )
 
 SCREEN = pygame.display.set_mode(SCREEN_SIZE) #crear la pantalla (800 ancho, 600 de alto)
 fondo = pygame.image.load("imagenes/ciudad.jpg")
-pygame.display.set_allow_screensaver("Mi primer juego") # Cambiar titulo
+icon = pygame.image.load("imagenes/escudo_icon.png")
+pygame.display.set_caption("Mi primer juego") # Cambiar titulo
+pygame.display.set_icon(icon)
 
 
 SCREEN.fill(CUSTOM) # Cambiar color de la pantalla .fill((red, green, blue)) maximo de valor de color es 255
 SCREEN.blit(fondo,(0,0))
-contador = 0
+# contador = 0
 
 is_running = True # bandera para decir que esta andando el progragama
 while is_running:
-    print(contador)
-    contador +=1
+    # print(contador)
+    # contador +=1 | contador para ver cuanto se itera
     for event in pygame.event.get(): # pygame.event.get() # Devuelve una lista de los eventos que ocurrieron
         if event.type == pygame.QUIT: # pygame.QUIT = 256 numero del evento para cerrar (quit) | Todos los eventos tienen un valor numerico y aparecen como constant
             is_running = False
--- src/settings.py
+++ src/settings.py
@@ -1,9 +1,9 @@
 
-
-ANCHO = 800
+# Screen
+ANCHO = 1000
 ALTO = 600
 SCREEN_SIZE = (ANCHO, ALTO)
-
+# 1000, 600
 # Colors
 
 RED = (255, 0, 0)

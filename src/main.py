ea756a350397d650360d651b7f2ee540373cc4c6
luporojo <alejoyerban112@gmail.com>
2024-06-13 18:31:37 -0300

    Se vienen cositas

--- src/main.py
+++ src/main.py
@@ -1,29 +1,35 @@
 import pygame
-from settings import * # Cosas para los settings 
+from settings import * # Cosas para las settings 
 # import sys (no le gusta al profe)
 # sys.exit() # escapar del programa
 
 pygame.init() # Inicia todos los modulos (.display, .mixer, etc )
 
 SCREEN = pygame.display.set_mode(SCREEN_SIZE) #crear la pantalla (800 ancho, 600 de alto)
-fondo = pygame.image.load("imagenes/ciudad.jpg")
+fondo = pygame.image.load("imagenes/ciudad_bien.png").convert() #.convert se utiliza para optimizar las imagenes y no usar tantos recursos
 icon = pygame.image.load("imagenes/escudo_icon.png")
-pygame.display.set_caption("Mi primer juego") # Cambiar titulo
+pygame.display.set_caption("Am I a test?") # Cambiar titulo
 pygame.display.set_icon(icon)
 
 
 SCREEN.fill(CUSTOM) # Cambiar color de la pantalla .fill((red, green, blue)) maximo de valor de color es 255
-SCREEN.blit(fondo,(0,0))
+SCREEN.blit(fondo,(x,y))
 # contador = 0
 
 is_running = True # bandera para decir que esta andando el progragama
 while is_running:
     # print(contador)
-    # contador +=1 | contador para ver cuanto se itera
-    for event in pygame.event.get(): # pygame.event.get() # Devuelve una lista de los eventos que ocurrieron
-        if event.type == pygame.QUIT: # pygame.QUIT = 256 numero del evento para cerrar (quit) | Todos los eventos tienen un valor numerico y aparecen como constant
+    # contador += 1 # contador para ver cuanto se itera
+    for event in pygame.event.get(): # pygame.event.get() devuelve una lista de los eventos que ocurrieron
+        if event.type == pygame.QUIT: # pygame.QUIT = 256, numero del evento para cerrar (quit) | Todos los eventos tienen un valor numérico y aparecen como constante
             is_running = False
-
-    pygame.display.flip() # Voltea la pantall \ actualiza
+    
+    x_relativa = x % fondo.get_rect().width
+    SCREEN.blit(fondo, (x_relativa - fondo.get_rect().width, y))
+    if x_relativa < ANCHO:
+        SCREEN.blit(fondo, (x_relativa, y))
+    x -= 0.5 # Velocidad del fondo
+    # RELOJ.tick(FPS)
+    pygame.display.flip() # Voltea la pantalla | actualiza
 
 pygame.quit() # contrario a pygame.init() | Cierra el programa
--- src/settings.py
+++ src/settings.py
@@ -1,12 +1,26 @@
+import pygame
 
 # Screen
 ANCHO = 1000
 ALTO = 600
 SCREEN_SIZE = (ANCHO, ALTO)
 # 1000, 600
-# Colors
+# Colors 2467x1696
 
 RED = (255, 0, 0)
 GRENN = (0, 255, 0)
 BLUE = (0, 0, 255)
 CUSTOM = (157, 208, 230)
+
+# Background
+
+y = 0
+x = 0
+
+# Performance
+
+FPS = 60
+RELOJ = pygame.time.Clock()
+
+# FUNCIONES DE TECLAS
+
